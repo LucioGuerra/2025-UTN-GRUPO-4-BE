46f96fe7d486bee5b2e20eec4dfb5145f4a290ab
Lucio Guerra <lucioguerra2001@gmail.com>
2025-08-30 11:02:19 -0300

    Creacion del proyecto

---
+++ src/test/java/org/agiles/bolsaestudiantil/BolsaestudiantilApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agiles.bolsaestudiantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaestudiantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
