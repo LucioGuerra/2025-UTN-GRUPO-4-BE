fc7c18631e0216bf22df0d775c8b767863653d83
Lucio Guerra <lucioguerra2001@gmail.com>
2025-08-30 11:02:19 -0300

    Creacion del proyecto

---
+++ src/main/java/org/agiles/bolsaestudiantil/BolsaestudiantilApplication.java
@@ -0,0 +1,13 @@
+package org.agiles.bolsaestudiantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsaestudiantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsaestudiantilApplication.class, args);
+	}
+
+}
