6f3b818f0c03cca5c645178d35c8a5344574db70
Lucio Guerra <lucioguerra2001@gmail.com>
2025-10-02 11:23:14 -0300

    Configuracion minima de mappers

---
+++ src/main/java/org/agiles/bolsaestudiantil/mapper/OfferMapper.java
@@ -0,0 +1,12 @@
+package org.agiles.bolsaestudiantil.mapper;
+
+import org.agiles.bolsaestudiantil.dto.response.OfferDTO;
+import org.agiles.bolsaestudiantil.entity.OfferEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = {StudentMapper.class})
+public interface OfferMapper {
+    OfferDTO toDTO(OfferEntity entity);
+
+    OfferEntity toEntity(OfferDTO dto);
+}
