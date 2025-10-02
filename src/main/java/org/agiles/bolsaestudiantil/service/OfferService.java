48a5cd97c7f80b8ceebc28a2abe583947b319bc1
Lucio Guerra <lucioguerra2001@gmail.com>
2025-10-02 11:17:13 -0300

    logica minima para el service

--- src/main/java/org/agiles/bolsaestudiantil/service/OfferService.java
+++ src/main/java/org/agiles/bolsaestudiantil/service/OfferService.java
@@ -1,7 +1,10 @@
 package org.agiles.bolsaestudiantil.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.agiles.bolsaestudiantil.dto.response.OfferDTO;
+import org.agiles.bolsaestudiantil.entity.OfferEntity;
+import org.agiles.bolsaestudiantil.entity.StudentEntity;
 import org.agiles.bolsaestudiantil.repository.OfferRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +13,16 @@ import org.springframework.stereotype.Service;
 public class OfferService {
 
     private final OfferRepository offerRepository;
+    private final StudentService studentService;
 
     public OfferDTO applyToOffer(Long offerId, Long studentId) {
-        return new OfferDTO();
+        StudentEntity student = studentService.findById(studentId);
+        OfferEntity offer = offerRepository.findById(offerId).orElseThrow(() -> new EntityNotFoundException("Offer not found with id: " + offerId));
+
+        offer.addStudent(student);
+
+        offerRepository.save(offer);
+
+        return mapToDTO(offer);
     }
 }
