ccc3a2538fb8c88dcdfd05cbcb72d3ce9a806201
Lucio Guerra <lucioguerra2001@gmail.com>
2025-10-02 11:29:24 -0300

    Logica basica hecha pero sin testea

---
+++ src/main/java/org/agiles/bolsaestudiantil/controller/OfferController.java
@@ -0,0 +1,21 @@
+package org.agiles.bolsaestudiantil.controller;
+
+import lombok.AllArgsConstructor;
+import org.agiles.bolsaestudiantil.dto.response.OfferDTO;
+import org.agiles.bolsaestudiantil.service.OfferService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/offers")
+public class OfferController {
+
+    private final OfferService offerService;
+
+    @PostMapping("/{offer_id}/apply")
+    public ResponseEntity<OfferDTO> applyToOffer(@RequestParam Long student_id, @PathVariable Long offer_id){
+        return ResponseEntity.status(HttpStatus.OK).body(offerService.applyToOffer(student_id, offer_id));
+    }
+}
