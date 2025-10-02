888cefcc99999a8e1c0bc98f7eb9a7f568f57111
MarianSalasT <marianosalas24@gmail.com>
2025-10-02 17:32:18 -0300

    Mappers funcionando

--- src/main/java/org/agiles/bolsaestudiantil/mapper/OfferMapper.java
+++ src/main/java/org/agiles/bolsaestudiantil/mapper/OfferMapper.java
@@ -1,13 +1,22 @@
 package org.agiles.bolsaestudiantil.mapper;
 
+import org.agiles.bolsaestudiantil.dto.request.OfferRequestDTO;
 import org.agiles.bolsaestudiantil.dto.response.OfferDTO;
+import org.agiles.bolsaestudiantil.dto.response.OfferResponseDTO;
 import org.agiles.bolsaestudiantil.entity.OfferEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {StudentMapper.class})
+@Mapper(componentModel = "spring", uses = {StudentMapper.class, OfferAttributeMapper.class})
 public interface OfferMapper {
     OfferDTO toDTO(OfferEntity entity);
 
     OfferEntity toEntity(OfferDTO dto);
+
+    OfferResponseDTO toOfferResponseDTO(OfferEntity entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "students", ignore = true)
+    @Mapping(target = "attributes", ignore = true)
+    OfferEntity createOfferRequestToEntity(OfferRequestDTO dto);
 }
