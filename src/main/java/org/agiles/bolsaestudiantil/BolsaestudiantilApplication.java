ec8b8ba276c90aa940443eb09233606586da913a
Lucio Guerra <lucioguerra2001@gmail.com>
2025-10-19 17:46:46 -0300

    Creacion de cliente feign para comunicar con kaycloak

--- src/main/java/org/agiles/bolsaestudiantil/BolsaestudiantilApplication.java
+++ src/main/java/org/agiles/bolsaestudiantil/BolsaestudiantilApplication.java
@@ -2,8 +2,10 @@ package org.agiles.bolsaestudiantil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication()
+@EnableFeignClients(basePackages = "org.agiles.bolsaestudiantil.client")
 public class BolsaestudiantilApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/org/agiles/bolsaestudiantil/configuration/FeignConfig.java
@@ -0,0 +1,20 @@
+package org.agiles.bolsaestudiantil.configuration;
+
+import feign.Logger;
+import feign.Request;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public Request.Options options() {
+        return new Request.Options(5000, 10000);
+    }
+}
