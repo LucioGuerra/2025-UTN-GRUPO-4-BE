019385b0a9302f51126e8f4420a391bdd78cb2e5
Lucio Guerra <lucioguerra2001@gmail.com>
2025-10-21 16:15:29 -0300

    Register con roles funcionando

--- src/main/java/org/agiles/bolsaestudiantil/client/KeycloakClient.java
+++ src/main/java/org/agiles/bolsaestudiantil/client/KeycloakClient.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @FeignClient(
@@ -31,4 +32,33 @@ public interface KeycloakClient {
 
     @PostMapping(value = "/realms/master/protocol/openid-connect/token", consumes = "application/x-www-form-urlencoded")
     Map<String, Object> getAdminToken(@RequestBody MultiValueMap<String, String> form);
+
+    @GetMapping("/admin/realms/{realm}/users/{userId}/role-mappings/realm")
+    List<Map<String, Object>> getUserRealmRoles(
+            @RequestHeader("Authorization") String bearerToken,
+            @PathVariable("realm") String realm,
+            @PathVariable("userId") String userId
+    );
+
+    @PostMapping("/admin/realms/{realm}/users/{userId}/role-mappings/realm")
+    void assignRole(
+            @RequestHeader("Authorization") String bearerToken,
+            @PathVariable("realm") String realm,
+            @PathVariable("userId") String userId,
+            @RequestBody List<Map<String, Object>> roles
+    );
+
+    @GetMapping("/admin/realms/{realm}/roles/{roleName}")
+    Map<String, Object> getRoleByName(
+            @RequestHeader("Authorization") String bearerToken,
+            @PathVariable("realm") String realm,
+            @PathVariable("roleName") String roleName
+    );
+
+    @GetMapping("/admin/realms/{realm}/users")
+    List<Map<String, Object>> getUsersByUsername(
+            @RequestHeader("Authorization") String bearerToken,
+            @PathVariable("realm") String realm,
+            @RequestParam("username") String username
+    );
 }
