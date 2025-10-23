4738f27c992c956bcb6b6967a0dd1af4a83cf38e
Lucio Guerra <lucioguerra2001@gmail.com>
2025-10-23 17:58:57 -0300

    Eliminado lo viejo

--- src/main/java/org/agiles/bolsaestudiantil/repository/pre_refactor/AplicanteRepository.java
+++
@@ -1,9 +0,0 @@
-package org.agiles.bolsaestudiantil.repository.pre_refactor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AplicanteRepository extends JpaRepository<AplicanteEntity, Long>, JpaSpecificationExecutor<AplicanteEntity> {
-}
