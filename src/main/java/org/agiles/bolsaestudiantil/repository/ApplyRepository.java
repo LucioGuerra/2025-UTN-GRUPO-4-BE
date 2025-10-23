c77513d059f7678dd60308ccefe854af2d166a93
Lucio Guerra <lucioguerra2001@gmail.com>
2025-10-23 17:02:07 -0300

    Apply hecho

--- src/main/java/org/agiles/bolsaestudiantil/repository/ApplyRepository.java
+++ src/main/java/org/agiles/bolsaestudiantil/repository/ApplyRepository.java
@@ -2,8 +2,9 @@ package org.agiles.bolsaestudiantil.repository;
 
 import org.agiles.bolsaestudiantil.entity.ApplyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApplyRepository extends JpaRepository<ApplyEntity,Long> {
+public interface ApplyRepository extends JpaRepository<ApplyEntity,Long>, JpaSpecificationExecutor<ApplyEntity> {
 }
