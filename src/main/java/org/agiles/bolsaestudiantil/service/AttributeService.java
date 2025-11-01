064309143fbfc21c8ab103aea91c0c5546a37e93
Lucio Guerra <lucioguerra2001@gmail.com>
2025-11-01 17:00:25 -0300

    filtrado dinamico de atributos andando

--- src/main/java/org/agiles/bolsaestudiantil/service/AttributeService.java
+++ src/main/java/org/agiles/bolsaestudiantil/service/AttributeService.java
@@ -1,15 +1,21 @@
 package org.agiles.bolsaestudiantil.service;
 
 import lombok.AllArgsConstructor;
+import org.agiles.bolsaestudiantil.dto.internal.AttributeFilter;
+import org.agiles.bolsaestudiantil.dto.response.AttributeResponseDTO;
 import org.agiles.bolsaestudiantil.entity.AttributeEntity;
+import org.agiles.bolsaestudiantil.mapper.AttributeMapper;
 import org.agiles.bolsaestudiantil.repository.AttributeRepository;
+import org.agiles.bolsaestudiantil.specification.AttributeSpecification;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AttributeService {
 
-    private AttributeRepository attributeRepository;
+    private final AttributeRepository attributeRepository;
+    private final AttributeMapper attributeMapper;
 
     public AttributeEntity findOrCreateAttribute(String name) {
         return attributeRepository.findByName(name)
@@ -20,5 +26,8 @@ public class AttributeService {
                 });
     }
 
-
+    public List<AttributeResponseDTO> getAllAttributes(AttributeFilter filter) {
+        List<AttributeEntity> attributes = attributeRepository.findAll(AttributeSpecification.withFilters(filter));
+        return attributes.stream().map(attributeMapper::toResponseDTO).toList();
+    }
 }
