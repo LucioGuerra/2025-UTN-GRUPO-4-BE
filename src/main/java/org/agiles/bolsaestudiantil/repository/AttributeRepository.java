ca154dfbad11770f2a230db486ffc8be86375df0
Lucio Guerra <lucioguerra2001@gmail.com>
2025-11-01 17:00:25 -0300

    filtrado dinamico de atributos andando

--- src/main/java/org/agiles/bolsaestudiantil/repository/AttributeRepository.java
+++ src/main/java/org/agiles/bolsaestudiantil/repository/AttributeRepository.java
@@ -2,11 +2,12 @@ package org.agiles.bolsaestudiantil.repository;
 
 import org.agiles.bolsaestudiantil.entity.AttributeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AttributeRepository extends JpaRepository<AttributeEntity, Long> {
+public interface AttributeRepository extends JpaRepository<AttributeEntity, Long>, JpaSpecificationExecutor<AttributeEntity> {
     Optional<AttributeEntity> findByName(String name);
 }
