a9138cc8fce2468162a1c6b66498e1734c7583ba
Lucio Guerra <lucioguerra2001@gmail.com>
2025-11-01 17:18:53 -0300

    Busqueda de usuarios andando

--- src/main/java/org/agiles/bolsaestudiantil/controller/UserController.java
+++ src/main/java/org/agiles/bolsaestudiantil/controller/UserController.java
@@ -1,8 +1,11 @@
 package org.agiles.bolsaestudiantil.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.agiles.bolsaestudiantil.dto.internal.UserFilter;
 import org.agiles.bolsaestudiantil.dto.response.UserResponseDTO;
 import org.agiles.bolsaestudiantil.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,18 @@ public class UserController {
     @GetMapping("/keycloak/{keycloakId}")
     public ResponseEntity<UserResponseDTO> getUserByKeycloakId(@PathVariable String keycloakId) {
         UserResponseDTO response = userService.getUserByKeycloakId(keycloakId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<UserResponseDTO>> searchUsers(
+            @RequestParam String search,
+            Pageable pageable) {
+        
+        UserFilter filter = new UserFilter();
+        filter.setSearch(search);
+        
+        Page<UserResponseDTO> response = userService.searchUsers(filter, pageable);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/org/agiles/bolsaestudiantil/repository/UserRepository.java
+++ src/main/java/org/agiles/bolsaestudiantil/repository/UserRepository.java
@@ -2,8 +2,26 @@ package org.agiles.bolsaestudiantil.repository;
 
 import org.agiles.bolsaestudiantil.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, String> {
+public interface UserRepository extends JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
+    
+    @Query("SELECT u FROM UserEntity u WHERE " +
+           "LOWER(CONCAT(u.name, ' ', u.surname)) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
+           "LOWER(u.name) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
+           "LOWER(u.surname) LIKE LOWER(CONCAT('%', :search, '%')) " +
+           "ORDER BY " +
+           "CASE " +
+           "  WHEN LOWER(CONCAT(u.name, ' ', u.surname)) LIKE LOWER(CONCAT(:search, '%')) THEN 1 " +
+           "  WHEN LOWER(u.name) LIKE LOWER(CONCAT(:search, '%')) THEN 2 " +
+           "  WHEN LOWER(u.surname) LIKE LOWER(CONCAT(:search, '%')) THEN 3 " +
+           "  ELSE 4 " +
+           "END")
+    Page<UserEntity> findByNameOrSurnameContaining(@Param("search") String search, Pageable pageable);
 }
